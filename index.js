4e4f107dbfd975a9084d86e1f9a05da2c50acd5d
sirojbek <sirojdevx@gmail.com>
2025-07-11 11:05:36 +0500

    fix: fixed size, and resolution

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const qualities = [
   program
     .name('ffgen')
     .description('CLI to generate and run FFmpeg commands 💻🎬')
-    .version('1.2.1', '-v, --version', 'Display version information')
+    .version('1.2.2', '-v, --version', 'Display version information')
 
     .argument('<input>', 'input video file (.mp4)')
     .argument('<output>', 'output folder')
@@ -112,7 +112,7 @@ const qualities = [
 
       function generate(resolution, audioIndex) {
         const validQualities = qualities.filter(
-          (q) => q.width <= resolution.width && q.height <= resolution.height
+          (q) => q.width <= resolution.width || q.height <= resolution.height
         );
         console.log(`Available qualities: ${validQualities.map(q => q.name).join(', ')}`);
 
@@ -125,7 +125,7 @@ const qualities = [
 
         const logoFilter = logo
           ? validQualities
-          .map((q, i) => `[1:v]scale=${Math.floor(q.width / 8)}:-1[logo${i + 1}]`)
+          .map((q, i) => `[1:v]scale=180:-1[logo${i + 1}]`)
           .join(';') + ';'
           : '';
 
